bd88e35789091edb4bd8ff0b1f7b3f72162794c8
saeedalipanah <saeedalipanah1374@gmail.com>
2025-05-20 17:07:44 +0330

    fix: test router2

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,9 +2,11 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/Home.vue'
 // @ts-ignore
 import AuthCallback from '../views/AuthCallback.vue'
+// @ts-ignore
+import ReturnNavigation from '../views/ReturnNavigation.vue'
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHistory('/'),
   routes: [
     {
       path: '/',
@@ -12,7 +14,7 @@ const router = createRouter({
       component: HomeView,
     },
     { path: '/auth-callback', component: AuthCallback },
-    { path: '/return-navigation', component: () => import('../views/ReturnNavigation.vue' as any) },
+    { path: '/return-navigation', component: ReturnNavigation },
   ],
 })
 
