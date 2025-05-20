b517a2a9524cdca1165706338d14560393255288
saeedalipanah <saeedalipanah1374@gmail.com>
2025-05-20 15:57:43 +0330

    fix: remove incodeing

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,7 @@ export default {
   methods: {
     startCanva() {
       const clientId = 'OC-AZbnBvE3oLbV' // کلاینت آیدی از کانوا
-      const redirectUri = encodeURIComponent(window.location.origin + '/auth-callback')
+      const redirectUri = window.location.origin + '/auth-callback'
 
       const canvaUrl = `https://www.canva.com/oauth2/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=code&scope=designs.read+designs.write`
 
